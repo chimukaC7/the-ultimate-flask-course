49bfcec3b5246de8e29e23742471271ea8b10907
CCMoonde <ccmoonde@zesco.co.zm>
2026-05-02 20:08:01 +0200

    Refactor form.html for improved HTML structure and readability; add git_toolbox_prj.xml for GitToolBox settings

--- Section5-RegistrationApp/5 Registration App/11 Error Handling/registration_form/registration_form/views.py
+++ Section5-RegistrationApp/5 Registration App/11 Error Handling/registration_form/registration_form/views.py
@@ -16,7 +16,7 @@ def index(member_id):
         member = Member.query.get_or_404(member_id)
 
     errors = {}
-        
+
     if request.method == 'POST':
         # Gather Form Input
         email = request.form['email']
@@ -26,7 +26,7 @@ def index(member_id):
         fav_language = request.form['fav_language']
         about = request.form['about']
         learn_new_interest = request.form['learn_new_interest']
-        interest_in_topics = request.form.getlist('interest_in_topics')
+        interest_in_topics = request.form.getlist('interest_in_topics') # getlist() is used to get all values of a multi-select field
 
         if not email:
             errors['email'] = 'You must have an email address.'
@@ -43,7 +43,7 @@ def index(member_id):
 
         if not errors:
             if member:
-                member.email = email 
+                member.email = email
 
                 if password:
                     member.password = password
@@ -52,23 +52,19 @@ def index(member_id):
                 member.first_learn_date = datetime.strptime(first_learn_date, '%Y-%m-%d')
                 member.fav_language = fav_language
                 member.about = about
-                member.learn_new_interest = (
-                    True if learn_new_interest == 'yes' else False
-                )
+                member.learn_new_interest = (True if learn_new_interest == 'yes' else False)
 
                 member.interest_in_topics[:] = []
-            
+
             else:
                 member = Member(
                     email = email,
                     password = password,
                     location = location,
-                    first_learn_date = datetime.strptime(first_learn_date, '%Y-%m-%d'),
+                    first_learn_date = datetime.strptime(first_learn_date, '%Y-%m-%d'),# The datetime.strptime() function is used to convert the string input from the form into a datetime object that can be stored in the database.
                     fav_language = fav_language,
                     about = about,
-                    learn_new_interest = (
-                        True if learn_new_interest == 'yes' else False
-                    )
+                    learn_new_interest = (True if learn_new_interest == 'yes' else False)
                 )
 
                 db.session.add(member)
@@ -84,6 +80,7 @@ def index(member_id):
     languages = Language.query.all()
     topics = Topic.query.all()
 
+    # If there are errors, we want to re-render the form with the previously entered data and the error messages.
     context = {
         'member_id' : member_id,
         'languages' : languages,
@@ -92,4 +89,4 @@ def index(member_id):
         'errors' : errors
     }
 
-    return render_template('form.html', **context)
+    return render_template('form.html', **context)# This code defines a Flask Blueprint for the main routes of the application. It includes a route for the index page, which can handle both GET and POST requests. The route can also accept an optional member_id parameter, which is used to retrieve a specific member from the database.
